4be97b69f2a72a2a5ecf19de06296e68cfe54230
matth-ew-B <77402287+matth-ew-B@users.noreply.github.com>
2021-01-14 10:59:12 -0500

    Add files via upload

--- BlackJackGame.java
+++ BlackJackGame.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package casino;
-
+import java.util.ArrayList;
 /**
  *
  * @author matth
@@ -22,6 +22,23 @@ public class BlackJackGame {
                 System.out.println(card[i][j].name);
             }
         }
+    }
+    public static ArrayList<cards> shuffleCards(int[][] cardAr){
+        ArrayList<cards> shuffledCards= new ArrayList();
+        
+        for (int i = 0; i < 52; i++) {// 52 cards in deck
+            int suit = (int)(Math.random() * (4 - 1 + 1) + 1)-1;// for every suit
+            int num = (int)(Math.random() * (13 - 1 + 1) + 1)-1;// for every num in suit
+            while (cardAr[suit][num].isShuffled == true){//making sure no repeats
+                suit = (int)(Math.random() * (4 - 1 + 1) + 1)-1;//random num for suit
+                num = (int)(Math.random() * (13 - 1 + 1) + 1)-1;//random num for num
+            }
+            cardAr[suit][num].isShuffled = true;//switch to true
+            cards addCard= new cards (suit, num);
+            shuffledCards.add(addCard);//add new card to shuffled deck
+        }
+        
+        return shuffledCards;
     }
     
 }
